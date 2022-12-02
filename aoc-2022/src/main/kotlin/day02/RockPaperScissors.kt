d5c6d069fdc0bcd200d61c71bce5acfa4e63e685
David Williams <davwil00@gmail.com>
2022-12-02 22:10:37 +0000

    Refactor day 2

--- aoc-2022/src/main/kotlin/day02/RockPaperScissors.kt
+++ aoc-2022/src/main/kotlin/day02/RockPaperScissors.kt
@@ -12,75 +12,66 @@ class RockPaperScissors(private val input: List<String>) {
     private val strategy = parseStrategy()
 
     private fun parseStrategy(): Strategy = input.map { line ->
-            val (move, response) = line.split(" ")
-            val opponentShape = Shape.fromEncodedValue(move)
-            val responseShape = Shape.fromEncodedValue(response)
-            Pair(opponentShape, responseShape)
+            val (opponentShape, response) = line.split(" ")
+            Pair(opponentShape.toShape(), response)
         }
 
-    fun calculateScore() = strategy.sumOf { move ->
-        val outcomeScore = getOutcome(move).score
-        move.second.score + outcomeScore
+    fun calculateScore() = strategy.sumOf { round ->
+        val yourShape = round.second.toShape()
+        val outcomeScore = getOutcome(round.first, yourShape).score
+        yourShape.score + outcomeScore
     }
 
     fun calculateScoreForDesiredOutcome() = strategy.sumOf { move ->
-        val desiredOutcome = Outcome.fromEncodedValue(move.second.encodedValue2)
+        val desiredOutcome = move.second.toOutcome()
         val moveScore = calculateMoveForOutcome(move.first, desiredOutcome).score
-        val outcomeScore = desiredOutcome.score
-        moveScore + outcomeScore
+        moveScore + desiredOutcome.score
+    }
+
+    private fun getOutcome(opponentShape: Shape, yourShape: Shape): Outcome = when {
+        yourShape winsAgainst opponentShape -> WIN
+        yourShape == opponentShape -> DRAW
+        opponentShape winsAgainst yourShape -> LOSE
+        else -> throw IllegalArgumentException("Unexpected scenario")
     }
 
     private fun calculateMoveForOutcome(opponentMove: Shape, desiredOutcome: Outcome) = when(desiredOutcome) {
-        WIN -> opponentMove.losesAgainst
+        WIN -> opponentMove.losesTo()
         DRAW -> opponentMove
         LOSE -> opponentMove.winsAgainst
-    }
-
-    private fun getOutcome(move: Move): Outcome = when {
-        move.second.winsAgainst == move.first -> WIN
-        move.second == move.first -> DRAW
-        move.second.losesAgainst == move.first -> LOSE
-        else -> throw IllegalArgumentException("Unexpected scenario")
     }
 
     enum class Outcome(val score: Int, val encodedValue: String) {
         WIN(6, "Z"),
         DRAW(3, "Y"),
         LOSE(0, "X");
-
-        companion object {
-            fun fromEncodedValue(encodedValue: String) = Outcome.values()
-                .first { it.encodedValue == encodedValue }
-        }
     }
+
+    private fun String.toOutcome() = Outcome.values().first { it.encodedValue == this }
 
     enum class Shape(val encodedValue1: String, val encodedValue2: String, val score: Int) {
         ROCK("A", "X", 1),
         PAPER("B", "Y", 2),
         SCISSORS("C", "Z", 3);
         lateinit var winsAgainst: Shape
-        lateinit var losesAgainst: Shape
 
         companion object {
             init {
                 ROCK.winsAgainst = SCISSORS
-                ROCK.losesAgainst = PAPER
-
                 PAPER.winsAgainst = ROCK
-                PAPER.losesAgainst = SCISSORS
-
                 SCISSORS.winsAgainst = PAPER
-                SCISSORS.losesAgainst = ROCK
             }
-
-            fun fromEncodedValue(encodedValue: String) = Shape.values()
-                .first { it.encodedValue1 == encodedValue || it.encodedValue2 == encodedValue }
         }
+
+        fun losesTo() = values().first { it.winsAgainst == this }
+        infix fun winsAgainst(other: Shape) = this.winsAgainst == other
     }
+
+    private fun String.toShape() = Shape.values().first { it.encodedValue1 == this || it.encodedValue2 == this }
 }
 
-typealias Move = Pair<Shape, Shape>
-typealias Strategy = List<Move>
+typealias Round = Pair<Shape, String>
+typealias Strategy = List<Round>
 
 fun main() {
     val rockPaperScissors = RockPaperScissors(readInputLines(2))
